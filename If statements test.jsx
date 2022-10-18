fecc92dc605b1be3a19e4d3ee575ea48584dd09f
abb22elikro <elias.kroon@abbgymnasiet.se>
2022-10-18 10:29:46 +0200

    första commit

---
+++ If statements test.jsx
@@ -0,0 +1,13 @@
+let hour = 10;
+if (hour >= 6 && hour < 12) {
+    alert ("Good morning");
+}
+else if (hour >= 12 && hour < 18) {
+    alert ("Good afternoon");
+}
+else if (hour >= 18 && hour < 24) {
+    alert ("Good evening");
+}
+else {
+    alert ("Go to sleep")
+}
